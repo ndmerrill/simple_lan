15cc7d75c6b0c728064bc851c756a9bea8b86a09
Nathan Merrill <nathan.d.merrill@gmail.com>
2014-11-11 17:40:37 -0800

    New code for UDP on client side. Should work but untested.

--- python2/client.py
+++ python2/client.py
@@ -1,15 +1,31 @@
 import socket
 import thread
 import json
+import time
 
 class Client(object):
-	"""docstring for Client"""
-	def __init__(self, name):
-		super(Client, self).__init__()
-		self.name = name
+    """docstring for Client"""
+    def __init__(self, name, port):
+        self.name = name
+        self.port = port
 
 
-	def get_server_list(self):
-		broadCastSock = 
-		servers = {}
+    def get_server_list(self, timeout=1):
+        """Get's the list of available servers on this port.
+        """
+        broadcast_sock = socket.socket(AF_INET, SOCK_DGRAM)
+        broadcast_sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+        servers = {}
+
+        for i in xrange(2):
+            broadcast_sock.sendto("to", ('<broadcast>', self.port))
+            start_time = time.time()
+            while (start_time - time.time() < timeout):
+                data, address = broadcast_sock.recvfrom(2)
+                if data == "fr":
+                    servers.append(address)
+
+        return servers
+
 
