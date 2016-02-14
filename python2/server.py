a92f776d0471460e1a4f36ead69d4d19203efbdc
rowanphipps <rowan.phipps@gmail.com>
2016-02-13 21:31:21 -0800

    progress on python server lobby

--- python2/server.py
+++ python2/server.py
@@ -1,6 +1,7 @@
 import socket
 import ipHelper
 import struct
+import Queue
 
 HOST = ipHelper.get_ip()
 PORT = 40393
@@ -19,3 +20,46 @@ while True:
     connecting.close()
 
 conn.close()
+
+
+class Server():
+	def __init__(self, port, name):
+		self.port = port
+		self.ip = ipHelper.get_ip()
+		self.name = name
+		self.players = {} # ip address -> (name, conn)
+		self.player_queue = Queue.queue()
+
+	def open_lobby(self, callback=None):
+		self.lobby = LobbyWorker(self.player_queue, self.port, self.name,
+			callback)
+
+	def pull_lobby(self):
+		while not self.player_queue.empty():
+			player = self.player_queue.get()
+			ip = player.pop(0)
+			self.players[ip] = players
+
+	def close_lobby(self);
+		self.pull_lobby()
+		self.lobby.join()
+
+
+class LobbyWorker(threading.Thread):
+	def __init__(self, player_queue, port, name, callback):
+		self.q = player_queue
+		self.callback = callback
+		self.name = name
+		self.port = port
+		self.stop_request = threading.Event()
+
+	def run(self):
+		udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		udp_sock.bind((ipHelper.get_ip(), self.port))
+
+		while not self.stop_request.isSet():
+			pass
+
+	def join(self, timeout=None):
+        self.stoprequest.set()
+        super(WorkerThread, self).join(timeout)
