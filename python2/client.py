4eb8c0592b5666b7cf0f9e9db6c8c8ea818f69f4
rowanphipps <rowan.phipps@gmail.com>
2016-02-13 19:38:20 -0800

    Added server discovery

--- python2/client.py
+++ python2/client.py
@@ -1,42 +1,83 @@
 import socket
 import ipHelper
 import threading
+import time
+import struct
 
 class Client():
 	def __init__(self, port):
 		self.port = port
 		self.connected = False
-		self.myIP = ipHelper.getIP()
+		self.myIP = ipHelper.get_ip()
 		self.server_discovery_port = socket.socket(
 			socket.AF_INET, socket.SOCK_DGRAM)
 
-	def getServerList(time=2000):
+	def getServerList(self, wait_time=1.5):
 		#  the subnet prefix is the first two elements from the client ip
 		#  address.  eg 192.168.
-		subnet_prefix = myIP.split(".")[0] + "." + myIP.split(".")[1] + "."
+		subnet_prefix = self.myIP.split(".")[0] + "." + self.myIP.split(".")[1] + "."
 
 		# this creates a list of /24 subnets to ping
-		subnets_24 = [subnet_prefix + i + "." for i in xrange(256)]
+		subnets_24 = [subnet_prefix + str(i) + "." for i in xrange(256)]
 		return_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		return_sock.bind((self.myIP, self.port))
+		return_sock.setblocking(0)
 		return_sock.listen(128)
 
 		data = ipHelper.pack_ip()
 
 		threads = []
 		for subnet in subnets_24:
-			t = threading.Thread(target=self._subnet_poller, ars=(subnet, data))
-		    threads.append(t)
-		    t.start()
+			t = threading.Thread(target=self._subnet_poller, args=(subnet, data))
+			threads.append(t)
+			t.start()
 
 		for t in threads:
 			t.join()
+
+		time.sleep(wait_time)
+
+		out = []
+
+		try:
+			while True:
+				conn, addr = return_sock.accept()
+				data = conn.recv(24)
+				conn.close()
+				ip = ipHelper.unpack_ip(data[0:4])
+				port = struct.unpack("I", data[4:8])[0]
+				name = data[8:].strip()
+				out.append((ip, port, name))
+				print ip, name, port
+
+		except socket.error as msg:
+			pass
+
+		finally:
+			try:
+				conn.close()
+			except :
+			pass
+
+		return out
 		
 
 
-	def _subnet_poller(target, data):
+	def _subnet_poller(self, target, data):
 		"""target is the /24 subnet targeted by this thread and data is the
 			data to be sent"""
+		# print "sends?"
 		for i in xrange(256):
-			self.server_discovery_port.sendto(data, (target+i, self.port))
+			try:
+				self.server_discovery_port.sendto(data, (target+str(i), self.port))
+			except socket.error as msg:
+				# print msg, target+str(i)
+				pass
+				# catches permission denied errors and ignores them because it
+				# doesn't matter if a couple of ip addresses don't work
+
 
 		
+if __name__ == '__main__':
+	c = Client(40393)
+	print c.getServerList()
